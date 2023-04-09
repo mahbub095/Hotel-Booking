0e78d19e16ecc24405c442977d707619e7bfce98
Mahbub <mahbub.095@gmail.com>
2023-04-10 00:11:31 +0600

    Hotel controller completed

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,68 +1,26 @@
 import express, { json } from "express";
-import Hotel from "../models/Hotel.js";
-import { createConnection } from "mongoose";
-import { createError } from "../utils/error.js";
-
+import {
+  createHotel,
+  deleteHotel,
+  getHotel,
+  getHotels,
+  updateHotel,
+} from "../controllers/hotel.js";
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req, res) => {
-  const newHotel = new Hotel(req.body);
-  try {
-    const saveHotel = await newHotel.save();
-    res.status(200).json(saveHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.post("/", createHotel);
 
 //UPDATE
-router.put("/:id", async (req, res) => {
-  try {
-    const updateHotel = await Hotel.findByIdAndUpdate(
-      req.params.id,
-      { $set: req.body },
-      { new: true }
-    );
-    res.status(200).json(updateHotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.put("/:id", updateHotel);
 
 //DELETE
-router.delete("/:id", async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has been deleted");
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.delete("/:id", deleteHotel);
 
 //GET
-router.get("/:id", async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get("/find/:id", getHotel);
 
 //GET ALL
-router.get("/", async (req, res, next) => {
-  // console.log("hi this is middleware");
-  // return next();
-  // const failed = true;
-
-  // if (failed) return next(createError(401, "You are not authenticated ! "));
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/", getHotels);
 
 export default router;
